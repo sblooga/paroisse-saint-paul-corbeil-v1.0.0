2f93d09c91d1f0b0336f1e8b1c0b56501e11bca9
Richard Szuszkiewicz <richard.szuszkiewicz@gmail.com>
2026-01-18 15:32:10 +0100

    18/01/26 test03 codex

--- server/db.js
+++ server/db.js
@@ -8,6 +8,12 @@ if (!connectionString) {
   throw new Error('DATABASE_URL manquant (variable vide ou non définie)');
 }
 
+// Logging non sensible pour diagnostiquer la connexion
+console.log(`[db] DATABASE_URL length=${connectionString.length}, prefix=${connectionString.slice(0, 12)}`);
+if (connectionString.includes(' ')) {
+  console.warn('[db] Attention: DATABASE_URL contient des espaces');
+}
+
 // Active le cache de connexion HTTP pour limiter le nombre de connexions côté Neon
 neonConfig.fetchConnectionCache = true;
 
